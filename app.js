c7132d376b7249ed0a2a41eef2d03c87823c7dcd
cAtshaw <swiftj101@hotmail.com>
2023-05-15 15:53:52 +0100

    Converter App Scrimba

---
+++ app.js
@@ -0,0 +1,17 @@
+const convertBtn = document.getElementById("convert-btn")
+const inputBtn = document.getElementById("enterbox")
+const lengthOutput = document.getElementById("length")
+const volumeOutput = document.getElementById("volume")
+const massOutput = document.getElementById("mass")
+
+// 1 meter = 3.281 feet
+// 1 liter = 0.264 gallon     Conversions
+// 1 kilogram = 2.204 pound
+
+
+// first create 
+
+
+convertBtn.addEventListener("click", () => {
+
+})
